bf224651435727c82d670307a74cb8e77eaf53e1
Seth Keim <skeim1@swarthmore.edu>
2021-09-04 12:02:39 -0400

    deleting old files

--- grass_seed_inc.py
+++
@@ -1,9 +0,0 @@
-costpsm = float(input())
-numlawns = int(input())
-
-sqmeters = 0
-for i in range(numlawns):
-    line = input().split()
-    sqmeters += float(line[0]) * float(line[1])
-
-print(sqmeters * costpsm)
